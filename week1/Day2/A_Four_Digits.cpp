13aa20fe654ea8eba3494babe43c33be161fe02e
Salman-Jewel <salmanjewel9966@gmail.com>
2024-10-28 01:13:47 +0600

    Add files via upload
    Add week1 day2

---
+++ week1/Day2/A_Four_Digits.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+    string s;cin>>s;
+    while(s.size()!=4){
+        s='0'+s;
+    }
+    cout<<s<<nl;
+
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    // int _;cin >> _;
+    // while (_--){
+        newbie();
+    // }
+    return 0;
+}
