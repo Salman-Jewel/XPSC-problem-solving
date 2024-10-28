44c46574d641ad69719e19ba94c06cc007a02f19
Salman-Jewel <salmanjewel9966@gmail.com>
2024-10-28 23:26:40 +0600

    Add files via upload
    Add week1 Day3

---
+++ week1/Day3/A_Counting.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+    int a,b;cin>>a>>b;
+    if(a<=b)cout<<b-a+1<<nl;
+    else cout<<0<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    // int _;cin >> _;
+    // while (_--){
+        newbie();
+    // }
+    return 0;
+}
---
+++ week1/Day3/A_Find_Multiple.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+    int a,b,c;cin>>a>>b>>c;
+    for(int i=a;i<=b;i++){
+        if(i%c==0){
+            cout<<i<<nl;return;
+        }
+    }
+    cout<<-1<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    // int _;cin >> _;
+    // while (_--){
+        newbie();
+    // }
+    return 0;
+}
