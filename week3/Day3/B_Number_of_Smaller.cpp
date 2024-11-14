8968fcf684a604b0c898fd1fb78b31c55566a1e9
Salman-Jewel <salmanjewel9966@gmail.com>
2024-11-14 15:43:07 +0600

    Add files via upload

--- week3/Day3/B_Number_of_Smaller.cpp
+++ week3/Day3/B_Number_of_Smaller.cpp
@@ -8,27 +8,20 @@ const int M=1e9+7;
     
 void newbie(){
     int n,m;cin>>n>>m;
-    vector<int>v(n);
-    for(int i=0;i<n;i++)cin>>v[i];
-    for(int i=0;i<m;i++){
-        int k;cin>>k;
-        int lo=0,hi=n-1,res=-1;
-        while(lo<=hi){
-            int mid=(hi+lo)/2;
-            if(v[mid]>k){
-                hi=mid-1;
-            }
-            else if(v[mid]<k){
-                lo=mid+1;
-            }
-            else {
-                res=mid;
-                hi=mid-1;
-            }
+    vector<int>a(n),b(m),ans(m);
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<m;i++)cin>>b[i];
+    int i=0,j=0,cnt=0;
+    while(j<m){
+        if(i<n && a[i]<b[j]){
+            i++;cnt++;
         }
-        if(res!=-1)cout<<res<<" ";
-        else cout<<lo<<" ";
+        else{
+            ans[j]=cnt;
+            j++;
+        }
     }
+    for(auto e : ans)cout<<e<<" ";
 }
     
 int main()
