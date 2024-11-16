eb93293ac79f33b4aa110041df836f57c7a39151
Salman-Jewel <salmanjewel9966@gmail.com>
2024-11-17 00:41:20 +0600

    Add files via upload
    Week 4 day1

---
+++ Day1/Football_Ties.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+    int x,y;cin>>x>>y;
+    cout<<x%3<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    int _;cin >> _;
+    while (_--){
+       newbie();
+    }
+    return 0;
+}
---
+++ Day1/RCB_vs_CSK.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+    int rcb,csk;cin>>rcb>>csk;
+    if(rcb-csk>=18)cout<<"RCB"<<nl;
+    else cout<<"CSK"<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    // int _;cin >> _;
+    // while (_--){
+        newbie();
+    // }
+    return 0;
+}
