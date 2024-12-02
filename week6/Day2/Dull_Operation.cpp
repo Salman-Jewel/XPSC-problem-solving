b43f76859d1ff316ea79e4d7a6fb6af32ebc95f5
Salman-Jewel <salmanjewel9966@gmail.com>
2024-12-02 22:59:09 +0600

    Add files via upload
    week 6 Day 2

---
+++ week6/Day2/Dull_Operation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+   ll n;cin>>n;
+//    int a=n,b=n-1;
+//    int c=(a|b)*(a^b);
+   cout<<n<<" "<<n-1<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    int _;cin >> _;
+    while (_--){
+       newbie();
+    }
+    return 0;
+}
