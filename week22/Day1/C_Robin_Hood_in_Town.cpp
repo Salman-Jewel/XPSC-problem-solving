49264f5501a228073c977263c51605639b249b7a
Salman-Jewel <salmanjewel9966@gmail.com>
2025-03-20 23:39:36 +0600

    Add files via upload
    week22 day1

---
+++ week22/Day1/C_Robin_Hood_in_Town.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define nl '\n'
+#define all(a) (a).begin(),(a).end()
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const ll N=1e6+10;
+const int M=1e9+7;
+    
+void newbie(){
+   int n;cin>>n;
+   vector<int>v(n);
+   for(int i=0;i<n;i++)cin>>v[i];
+   if(n<3){
+    cout<<-1<<nl;return;
+   }
+   sort(all(v));
+   ll s=accumulate(all(v),0LL);
+   ll x=(2LL*v[n/2]*n)-s+1;
+   cout<<max(0LL,x)<<nl;
+}
+    
+int main()
+{
+    ios::sync_with_stdio(false),
+    cin.tie(0),cout.tie(0);
+    int _;cin >> _;
+    while (_--){
+       newbie();
+    }
+    return 0;
+}
